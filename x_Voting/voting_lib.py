47096568de3f443f674523428cfcde0dd9e0cc42
Win Htut <winhtutonline@gmail.com>
2023-05-09 20:05:59 +0630

    day7-voting-project

---
+++ x_Voting/voting_lib.py
@@ -0,0 +1,126 @@
+class Voting:
+    def __init__(self):
+        print("Working in Voting special method or constructor ")
+        self.students = {0: {"name": "James", "v_mark": 0, "voter": []},
+                         1: {"name": "John", "v_mark": 0, "voter": []},
+                         2: {"name": "Rooney", "v_mark": 0, "voter": []},
+                         3: {"name": "Ronaldo", "v_mark": 0, "voter": []},
+                         4: {"name": "Messi", "v_mark": 0, "voter": []}
+                         }
+        self.db: dict = {}
+        self.id: int = 0
+
+        self.l_id: int = 0
+
+    def main_option(self):
+        option = 0
+        try:
+            option = int(input("Press 1 to Register\nPress 2 to Login\nPress 3 to Exit"))
+        except Exception as err:
+            # print(err)
+            print("Pls insert only Integer eg:1,2,3")
+
+        if option == 1:
+            self.register()
+        elif option == 2:
+            self.login()
+        elif option == 3:
+            exit(1)
+        else:
+            print("Invalid Option")
+            self.main_option()
+
+    def register(self):
+        print("This is register option ")
+        pass_match = False
+        try:
+            r_email = input("Enter your email address to register!")
+            r_name = input("Enter your name to register!")
+            r_phone = input("Enter your phone to register!")
+            r_address = input("Enter your address:")
+
+            while pass_match is False:
+                r_pass1 = input("Enter your password to register!")
+                r_pass2 = input("Retype your password:")
+
+                if r_pass1 != r_pass2:
+                    print("Your passwords not match")
+
+                else:
+                    print("Your passwords was recorded!")
+                    self.id = len(self.db)
+                    data_form: dict = {self.id: {"email": r_email, "name": r_name, "phone": r_phone,
+                                                 "address": r_address, "password": r_pass1}}
+
+                    self.db.update(data_form)
+
+                    pass_match = True
+        except Exception as err:
+            print("Invalid User Input!Try Again Sir!")
+            self.register()
+
+        print("Registration success :", self.db[self.id]["name"])
+
+        r_option = False
+        while r_option is False:
+            try:
+                user_option = int(input("Press 1 to Login!\nPress 2 Main Option:\nPress3 to Exit!:"))
+                if user_option == 1:
+                    self.login()
+                    break
+                elif user_option == 2:
+                    self.main_option()
+                    break
+                elif user_option == 3:
+                    exit(1)
+                else:
+                    print("Pls read again for option!")
+
+            except Exception as err:
+                print("Invalid Input!", err)
+
+    def login(self):
+        print("This is login option ")
+        length = len(self.db)
+        try:
+            l_email = input("Enter your email to Login:")
+            l_pass = input("Enter your pass to Login:")
+            self.l_id = -1
+            for i in range(length):
+                if l_email == self.db[i]["email"] and l_pass == self.db[i]["password"]:
+                    self.l_id = i
+                    break
+            if self.l_id != -1:
+                self.user_sector(self.l_id)
+            else:
+                print("Username or Password incorrect!")
+                self.login()
+
+        except Exception as err:
+            print(err, "\nInvalid input:")
+
+    def user_sector(self, l_id):
+        print("Welcome", self.db[l_id]["name"])
+
+        print("Please select one!")
+        for i in range(len(self.students)):
+            print("Id:{} - Name {} - Current Vote Mark: {}".format(i, self.students[i]["name"],
+                                                                   self.students[i]["v_mark"]
+                                                                   ))
+
+        try:
+            v_id = int(input("Just Enter Id number to vote:"))
+
+            self.students[v_id]["v_mark"] += 1
+
+            print("Congratulation you are voted!")
+            print("{} now voting mark is : {}".format(self.students[v_id]["name"],self.students[v_id]["v_mark"]))
+
+        except Exception as err:
+            print(err)
+
+
+# ဆက်ရေး ရန် 8-5-2023
+# voter များအား စာရင်း မှတ်ပေးရန်
+# file ထဲ တွင် အားလုံး သိမ်းရန်
+#
---
+++ x_Voting/voting_program.py
@@ -0,0 +1,5 @@
+from voting_lib import Voting
+
+if __name__ == '__main__':
+    voting = Voting()
+    voting.main_option()
